898a624a0c67e89b717a1949606d92f7b95633fc
jiayuylinfs <jiayu.li.nfs@gmail.com>
2017-07-22 12:38:35 -0300

    homework

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var s = " "
+let count = s.characters.count
+print(count)
---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Jiayu li on 2017-07-21.
+
+//  Copyright © 2017 Jiayu li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentyInDisplay = display.text!
+            display.text = textCurrentyInDisplay + digit
+        }
+        else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = calculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
+    
+    
+    
+    
+    
+}
+
